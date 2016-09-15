10c0becc9d56c4f755e6e16906a9b58513f8ba6c
J. Derek Tucker <jdtuck@sandia.gov>
2016-09-15 08:14:08 +0000

    version 1.7.0

--- R/fdasrvf-package.r
+++ R/fdasrvf-package.r
@@ -17,11 +17,14 @@
 #' @references Tucker, J. D. 2014, Functional Component Analysis and Regression using Elastic Methods. Ph.D. Thesis, Florida State University.
 #' @references Robinson, D. T. 2012, Function Data Analysis and Partial Shape Matching in the Square Root Velocity Framework. Ph.D. Thesis, Florida State University.
 #' @references Huang, W. 2014, Optimization Algorithms on Riemannian Manifolds with Applications. Ph.D. Thesis, Florida State University.
+#' @references Cheng, W., Dryden, I. L., and Huang, X. (2016). Bayesian registration of functions and curves. Bayesian Analysis, 11(2), 447-475.
 #' @references Srivastava, A., Klassen, E., Joshi, S., Jermyn, I., (2011). Shape analysis of elastic curves in euclidean spaces. Pattern Analysis and Machine Intelligence, IEEE Transactions on 33 (7), 1415-1428.
+#' @references Cheng, W., Dryden, I. L., and Huang, X. (2016). Bayesian registration of functions and curves. Bayesian Analysis, 11(2), 447-475.
 #' @docType package
 #' @useDynLib fdasrvf
 #' @import foreach mvtnorm matrixcalc splines parallel doParallel Rcpp fields
-#' @importFrom graphics layout legend matplot plot title
+#' @importFrom graphics layout legend matplot plot title lines
+#' @importFrom coda traceplot mcmc
 #' @importFrom stats approx cov optim predict quantile rnorm runif sd smooth.spline var spline
 #' @aliases fdasrvf fdasrvf-package
 NULL
